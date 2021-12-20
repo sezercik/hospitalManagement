765828bbad50f33f4bbcb8d2b63d244437af2ea2
sezercik <szrakoraler@gmail.com>
2021-12-20 23:04:45 +0300

    add text database

--- main.c
+++ main.c
@@ -9,53 +9,66 @@ char patient_disease[MAX];
 int patient_age;
 int i = 0;
 
-void create_patient(patient_name, patient_surname, patient_age,patient_disease)
+char *patient_db = "db/patient_db.txt";
+
+void create_patient(patient_name, patient_surname, patient_age, patient_disease)
 {
-	printf("Patient: %s %s %d created\nPatient Symptons is:%s\n\n\n",patient_name,patient_surname,patient_age,patient_disease);
+	FILE *fp = fopen(patient_db, "a");
+	if (fp == NULL)
+    {
+        printf("DB is not found: %s", patient_db);
+        return -1;
+    }
+
+    // write to the text file
+        fprintf(fp, "%s : %s | %s : %s | %s : %d | %s  : %s\n", "Patient Name",patient_name,"Patient Surname",patient_surname,"Patient Age",patient_age,"Patient Symptomps/Disase",patient_disease);
+
+    // close the file
+    fclose(fp);
 	
+	printf("Patient: %s %s %d created\nPatient Symptons is:%s\n\n\n", patient_name, patient_surname, patient_age, patient_disease);
 }
 
-main() {
-	
-do{
-	printf("Welcome to New Hospital\n");
-	printf("1) Start Patient  Admission Procedures\n");
-	printf("2) Edit a Patient File\n");
-	printf("3) Check for empty rooms\n");
-	printf("4) Check vaccination status for new babies\n");
-	printf("5) Look for medicine stock\n");
-	printf("6) Make an appoitment\n");
-	printf("7) Show Doctors\n");
+main()
+{
 
-	scanf("%d", &ch);
-
-	switch (ch)
+	do
 	{
-	case 1:
+		printf("Welcome to New Hospital\n");
+		printf("1) Start Patient  Admission Procedures\n");
+		printf("2) Edit a Patient File\n");
+		printf("3) Check for empty rooms\n");
+		printf("4) Check vaccination status for new babies\n");
+		printf("5) Look for medicine stock\n");
+		printf("6) Make an appoitment\n");
+		printf("7) Show Doctors\n");
+
+		scanf("%d", &ch);
+
+		switch (ch)
+		{
+		case 1:
 		{
 			printf("Patient name please...\n");
-			scanf("%s",patient_name);
-			
+			scanf(" %[^\n]s", patient_name);
+
 			printf("Patient Surname please...\n");
-			scanf("%s",patient_surname);
-			
+			scanf("%s", patient_surname);
+
 			printf("Patient Age please...\n");
-			scanf("%d",&patient_age);
-			
+			scanf("%d", &patient_age);
+
 			printf("Patient Symptoms/Disease please...\n");
-			scanf(" %[^\n]s",patient_disease);
-					
-			create_patient(patient_name, patient_surname, patient_age,patient_disease);
-			
+			scanf(" %[^\n]s", patient_disease);
+
+			create_patient(patient_name, patient_surname, patient_age, patient_disease);
+
 			sleep(5);
 			system("cls");
 		}
 		break;
-	default:
-		break;
-	}}
-while(i==0);
-
+		default:
+			break;
+		}
+	} while (i == 0);
 }
-
-
