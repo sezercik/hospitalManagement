6852183dc65a397e719e221f5024345bebf6d700
sezercik <you@example.com>
2021-12-22 00:15:50 +0300

    Changin database to sqlite but doesn't work for now.

--- main.c
+++ main.c
@@ -8,29 +8,22 @@ int ch;
 char patient_name[MAX];
 char patient_surname[MAX];
 char patient_disease[MAX];
+char patient_symptoms[MAX];
+
 int patient_age;
 int i = 0;
 
 char *patient_db = "db/patient_db.txt";
 
-void create_patient(patient_name, patient_surname, patient_age, patient_disease)
+create_patient(patient_name, patient_surname, patient_age, patient_disease, patient_symptoms)
 {
-	FILE *fp = fopen(patient_db, "a");
-	if (fp == NULL)
-    {
-        printf("DB is not found: %s", patient_db);
-        return -1;
-    }
-
-	printf("Patient: %s %s %d created\nPatient Symptons is:%s\n\n\n", patient_name, patient_surname, patient_age, patient_disease);
+	
+	//printf("Patient: %s %s %d created\nPatient Symptons is:%s\n\n\n", patient_name, patient_surname, patient_age, patient_disease);
 
     // write to the text file
-        fprintf(fp, "%s : %s | %s : %s | %s : %d | %s  : %s\n", "Patient Name",patient_name,"Patient Surname",patient_surname,"Patient Age",patient_age,"Patient Symptomps/Disase",patient_disease);
-		
-    // close the file
-    fclose(fp);
-	}
-
+		addToDatabase(patient_name,patient_surname,patient_age,patient_symptoms,patient_disease);
+	// fprintf(fp, "%s : %s | %s : %s | %s : %d | %s  : %s\n", "Patient Name",patient_name,"Patient Surname",patient_surname,"Patient Age",patient_age,"Patient Symptomps/Disase",patient_disease);
+}
 main()
 {
 
@@ -60,11 +53,14 @@ main()
 			printf("Patient Age please...\n");
 			scanf("%d", &patient_age);
 
-			printf("Patient Symptoms/Disease please...\n");
+			printf("Patient Disease please...\n");
 			scanf(" %[^\n]s", patient_disease);
 
-			create_patient(patient_name, patient_surname, patient_age, patient_disease);
+			printf("Patient Symptoms please...\n");
+			scanf(" %[^\n]s", patient_symptoms);
 
+			create_patient(patient_name, patient_surname, patient_age, patient_disease, patient_symptoms);
+			
 			sleep(5);
 			system("cls");
 		}
@@ -72,6 +68,7 @@ main()
 		case 2:
 		{
 			printf("Edit a Patient File\n");
+			
 
 		}
 		break;
@@ -79,5 +76,40 @@ main()
 			printf("Invalid choice\n");
 		}
 		}
-	} while (i == 0);
+		while (i == 0);
+	} 
+
+
+addToDatabase(patient_name,patient_surname,patient_age,patient_symptoms,patient_disease){
+	 
+    sqlite3 *db;
+    char *err_msg = 0;
+    
+    int rc = sqlite3_open("db/hospital.db", &db);
+    
+    if (rc != SQLITE_OK) {
+        
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+    }
+    
+    char *sql = "DROP TABLE IF EXISTS Patient;" 
+                "CREATE TABLE Cars(PatientId INT, PatientName STRING, PatientSurname STRING, PatientAge INT, PatientSymptoms TEXT, PatientDisease TEXT, PatientDoctor STRING);" 
+                "INSERT INTO Cars VALUES(patient_name,patient_surname,patient_age,patient_symptoms,patient_disease,'Prof. Dr. Mehmet sait');"; 
+              
+
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    
+    if (rc != SQLITE_OK ) {
+        
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        
+        sqlite3_free(err_msg);        
+        sqlite3_close(db);
+        
+
+    } 
+    
+    sqlite3_close(db);
 }
